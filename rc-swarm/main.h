110ffbf46673d0424eda498b25bff54ac6e61eb1
Anuj Kalia <anujkaliaiitd@gmail.com>
2017-12-31 05:20:25 -0500

    Reorg rc-swarm

--- rc-swarm/main.h
+++ rc-swarm/main.h
@@ -7,7 +7,13 @@
 #include "libhrd_cpp/hrd.h"
 #include "sweep.h"
 
-static_assert(is_power_of_two(kAppUnsigBatch), "");
+// kAppWindowSize is the number of outstanding requests kept by a worker thread
+// across all its QPs. This is only used for READs where we can detect
+// completion by polling on a READ's destination buffer.
+//
+// For WRITEs, this is hard to do unless we make every send() signaled. So,
+// the number of per-thread outstanding operations per thread with WRITEs is
+// O(NUM_CLIENTS * UNSIG_BATCH).
 
 static constexpr size_t kAppNumThreads = kAppNumWorkers / kAppNumMachines;
 static constexpr size_t kAppNumQPsPerThread =
@@ -25,20 +31,14 @@ static constexpr bool kAppRoundOffset = true;
 static constexpr size_t kAppPollingRegionSz = kAppWindowSize * kAppRDMASize;
 static_assert(kAppPollingRegionSz < kAppBufSize / 10, "");
 
-// static constexpr size_t kAppWindowSize = 32;
-// Number of outstanding requests kept by a worker thread  across all QPs.
-// This is only used for READs where we can detect completion by polling on
-// a READ's destination buffer.
-//
-// For WRITEs, this is hard to do unless we make every send() signaled. So,
-// the number of per-thread outstanding operations per thread with WRITEs is
-// O(NUM_CLIENTS * UNSIG_BATCH).
-
 static constexpr size_t kAppMaxPorts = 2;        // Max ports for a thread
 static constexpr size_t kAppMaxMachines = 256;   // Max machines in the swarm
 static constexpr int kAppWorkerBaseSHMKey = 24;  // SHM keys used by workers
 
 // Checks
+static_assert(kHrdMaxInline == 16, "");  // For single-cacheline WQEs
+static_assert(kAppRDMASize <= kHrdMaxInline, "");
+
 static_assert(kAppNumWorkers % kAppNumMachines == 0, "");
 static_assert(kAppBufSize >= MB(2), "");  // Large buffer, more parallelism
 static_assert(kAppNumMachines >= 2, "");  // At least 2 machines
@@ -58,17 +58,6 @@ DEFINE_uint64(base_port_index, 0, "Base port index");
 DEFINE_uint64(num_ports, 0, "Number of ports");
 DEFINE_uint64(machine_id, 0, "ID of this machine");
 DEFINE_uint64(do_read, 0, "Use RDMA READs?");
-
-static bool validate_do_read(const char*, uint64_t do_read) {
-  // Size checks based on opcode. This is not UD, so no 0-byte read/write.
-  if (do_read == 0) {  // For WRITEs
-    return kAppRDMASize >= 1 && kAppRDMASize <= kHrdMaxInline;
-  } else {
-    return kAppRDMASize >= 1 && kAppRDMASize <= kAppBufSize;
-  }
-}
-
-DEFINE_validator(do_read, &validate_do_read);
 
 // File I/O helpers
 
