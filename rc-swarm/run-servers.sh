76c4f3299b9e2585198f9e6c7a021ff94f399cec
Anuj Kalia <anujkaliaiitd@gmail.com>
2017-12-31 08:08:52 -0500

    Use nc to check for memcached status

--- rc-swarm/run-servers.sh
+++ rc-swarm/run-servers.sh
@@ -4,6 +4,8 @@ source $(dirname $0)/../scripts/mlx_env.sh
 export HRD_REGISTRY_IP="xia-wimpy"
 
 drop_shm
+exe="../build/rc-swarm"
+chmod +x $exe
 
 if [ "$#" -gt 2 -o "$#" -eq 0 ]; then
   blue "Illegal number of parameters"
@@ -15,27 +17,23 @@ fi
 if [ "$1" -eq 0 ]; then
 	blue "Machine 0: Resetting QP registry"
 	sudo killall memcached 1>/dev/null 2>/dev/null
+
+  # Spawn memcached, but wait for it to start
 	memcached -l 0.0.0.0 1>/dev/null 2>/dev/null &
-	sleep 1
+  while ! nc -z localhost 11211; do sleep .1; done
+  echo "Machine 0: memcached server is open for business on port 11211"
 fi
 
 blue "Machine $1: Starting worker threads"
-
-flags="
-  --machine_id $1 \
-  --base_port_index 0 \
-  --num_ports 2 \
-  --do_read 1
-"
-
 rm -f tput-out/*
 
+flags="--machine_id $1 $(cat config)"
 # Check for non-gdb mode
 if [ "$#" -eq 1 ]; then
-  sudo -E numactl --cpunodebind=0 --membind=0 ../build/rc-swarm $flags
+  sudo -E numactl --cpunodebind=0 --membind=0 $exe $flags
 fi
 
 # Check for gdb mode
 if [ "$#" -eq 2 ]; then
-  sudo -E gdb -ex run --args ../build/rc-swarm $flags
+  sudo -E gdb -ex run --args $exe $flags
 fi
